478f56794992232a58464f78d73e8498d7edc63d
Tim Conceivable <headskape@yahoo.com>
2019-12-06 02:54:29 -0600

    day5, pt 1- complete! uggghh

--- day02.py
+++ day02.py
@@ -10,20 +10,20 @@ def parse_data(filename):
 
 def run_code(code):
     for n in range(0,len(code)):
-        val = code[n]
-        if (n % 4 == 0) and (n+3 <= len(code)):
+        opcode = code[n]
+        if (n % 4 == 0) and (n+4 <= len(code)):
+            if opcode == 99:
+                #print(n,"OPCODE :",val,"/ STOP")
+                break
             p1 = code[n+1]
             p2 = code[n+2]
             p3 = code[n+3]
-            if val == 1:
+            if opcode == 1:
                 code[p3] = code[p1] + code[p2]
                 #print(n,"OPCODE :",code[p1],"+",code[p2])
-            if val == 2:
+            if opcode == 2:
                 code[p3] = code[p1] * code[p2]
                 #print(n,"OPCODE :",code[p1],"x",code[p2])
-            if val == 99:
-                #print(n,"OPCODE :",val,"/ STOP")
-                break
         else:
             pass #print(n,":",val)    
     return code[0]
--- day05.py
+++ day05.py
@@ -10,46 +10,36 @@ def parse_data(filename):
 def run_code(code,sys_id):
     i = 0
     for n in range(0,len(code)):
-        if n == i:
-            full_op = "{0:0=5d}".format(code[n])
-            print(n,full_op)
-            opcode = int(str(full_op)[-2:])
-            mode1 = int(str(full_op)[-3])
-            mode2 = int(str(full_op)[-4])
-            mode3 = int(str(full_op)[-5])
-            if opcode == 99:
-                print(n,"- STOP")
+        full_op = "{0:0=6d}".format(code[n]).strip('-')
+        opcode = int(str(full_op)[-2:])
+        if opcode == 99:
+                print("!") #(n,"- STOP")
                 break
-            else:
-                if (opcode <= 2) and (n+4 <= len(code)):
-                    p1 = code[n+1]
-                    if mode1 == 0: 
-                        p1 = code[code[n+1]]
-                    p2 = code[n+2]
-                    if mode2 == 0: 
-                        p2 = code[code[n+2]]
-                    p3 = code[n+3]
-                    if mode3 == 0: 
-                        p3 = code[code[n+3]]
-                    #print(n,code[n],"params:",mode1,":",p1,mode2,":",p2,mode3,":",p3)
-                    if opcode == 1:
-                        p3 = p1 + p2
-                        #print(n,code[n],"- op1:",full_op)
-                        i += 4
-                    if opcode == 2:
-                        p3 = p1 * p2
-                        #print(n,code[n],"- op2:",full_op)
-                        i += 4
-                elif (n+2 <= len(code)):
-                    p1 = code[n+1]
-                    if opcode == 3:
-                        code[p1] = sys_id
-                        #print(n,code[n],"- op3:",full_op)
-                        i += 2
-                    if opcode == 4:
-                        #print(n,code[n],"- op4:",full_op," - output:",code[p1])
-                        print(code[p1])
-                        i += 2
+        mode1 = int(str(full_op)[-3])
+        mode2 = int(str(full_op)[-4])
+        mode3 = int(str(full_op)[-5])
+        if n == i and (n+2 <= len(code)):
+            p1 = code[n+1]
+            if mode1 == 1:
+                p1 = n+1
+            if (opcode > 2):
+                if opcode == 3:
+                    code[p1] = sys_id
+                    i += 2
+                if opcode == 4:
+                    print(code[p1],"",end='')
+                    i += 2
+            elif (n+4 <= len(code)):
+                p2 = code[n+2]
+                if mode2 == 1:
+                    p2 = n+2
+                p3 = code[n+3]
+                if opcode == 1:
+                    code[p3] = code[p1] + code[p2]
+                    i += 4
+                if opcode == 2:
+                    code[p3] = code[p1] * code[p2]
+                    i += 4
     return code[0]
 
 
@@ -58,6 +48,7 @@ def main():
     sys_id = 1 #int(input("please enter the ID of the system to test:"))
     diagnostic = run_code(intcode,sys_id)
     #print(diagnostic)
+
 
 if __name__ == "__main__":
     main()
